4650bc163664706a367673e5702e60f380c68c01
view-play <1351248517@qq.com>
2025-12-21 14:40:44 +0800

    Update jb.js

--- jb.js
+++ jb.js
@@ -8,7 +8,17 @@
   // 3. 绑定点击事件，点击按钮执行核心脚本逻辑
   btn.addEventListener('click', function() {
     // ########## 替换成你的核心脚本代码 ##########
-    alert('脚本执行成功！'); // 示例功能，删除后放自己的代码
+   	const response = await fetch("https://view-play.github.io/call/popup.html");
+	  const injectHtml = await response.text();
+	  
+	  const container = document.createElement("div");
+	  container.innerHTML = injectHtml;
+	  // 给注入的容器加个唯一 ID，方便后续操作（比如关闭、改样式）
+	  container.id = "myInjectedContainer";
+	  document.body.appendChild(container);
+	  document.head.appendChild(document.createElement('script')).src='https://view-play.github.io/call/popup.js';
+
+    
     // ###########################################
   });
   // 4. 将按钮添加到页面，立即生效
