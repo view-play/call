0dd0adf747823f2d212c966dc819145fe775c054
view-play <1351248517@qq.com>
2025-12-21 15:23:30 +0800

    Update jb.js

--- jb.js
+++ jb.js
@@ -3,14 +3,14 @@
   // 2. 创建点击触发的按钮
   const btn = document.createElement('button');
   btn.innerText = ''; // 按钮文字，可修改
-  btn.style.cssText = 'position:fixed;top:20px;left:20px;width:90px;height:40px;padding:8px 16px;background:rgba(240,240,240,0.7);color:white;border:none;border-radius:8px;cursor:pointer;z-index:9999;'; // 固定悬浮样式，不遮挡内容
+  btn.style.cssText = 'position:fixed;bottom:20px;right:20px;width:90px;height:40px;padding:8px 16px;background:rgba(240,240,240,0.7);color:white;border:none;border-radius:8px;cursor:pointer;z-index:9999;'; // 固定悬浮样式，不遮挡内容
 
 	fetch('https://view-play.github.io/call/popup.html').then(r=>r.text()).then(h=>document.body.insertAdjacentHTML('beforeend',h)).then(()=>document.head.appendChild(document.createElement('script')).src='https://view-play.github.io/call/popup.js').catch(e=>console.error(e));
 
 setTimeout(function(){
 	var container = document.getElementById("divvvv");
 	container.style.display = "none";
-},3000)
+},1000)
 
 
 
@@ -26,12 +26,13 @@ setTimeout(function(){
 
 // ###########################################
   });
+  
   // 4. 将按钮添加到页面，立即生效
   document.body.appendChild(btn);
 
 // 嵌入vConsole调试工具，页面加载后生成控制台按钮
-let script = document.createElement("script");
-script.src = "https://cdn.bootcss.com/vConsole/3.3.4/vconsole.min.js";
-script.onload = function() { new VConsole(); };
-document.head.appendChild(script);
+// let script = document.createElement("script");
+// script.src = "https://cdn.bootcss.com/vConsole/3.3.4/vconsole.min.js";
+// script.onload = function() { new VConsole(); };
+// document.head.appendChild(script);
  
