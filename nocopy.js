826ccd7e805d802d17f85ea151197de9c30dd172
view-play <1351248517@qq.com>
2025-12-21 16:29:21 +0800

    Improve paste prevention functionality
    Refactor paste prevention logic in iframe and input elements.

--- nocopy.js
+++ nocopy.js
@@ -1,52 +1,3 @@
-		function c(doc){
-			for(var j =0; j < doc.getElementsByTagName("iframe").length;j++){
-			var  iframeDocument = doc.getElementsByTagName("iframe")[j].contentDocument;
-			if(iframeDocument.getElementById("tt")){
-				
-			     iframeDocument .getElementById("tt").parentNode.onselectstart=function(event) {
-		  event.returnValue = true;
-		};
-				 
-				 
-			}
-			 for (var i = 0; i < iframeDocument .getElementsByTagName("input").length; i++) {
-			      iframeDocument .getElementsByTagName("input")[i].onpaste=
-				  		(event) => {
-				  				  event.returnValue = true;
-				  				  return true; // 现代浏览器优先识别 return 值
-				  				};
-				  
-				  
-				  
-			 }			
-			c(iframeDocument);
-			}
-		
-			}
-			c(document);
-			console.log("执行完毕！");
-
-
-
-
-
- document.oncontextmenu = new Function("event.returnValue=true;");
- document.onselectstart = new Function("event.returnValue=true;");
-document.querySelectorAll('input,textarea').forEach(el=>{el.onpaste=null;el.addEventListener('paste',e=>e.stopImmediatePropagation());el.style.userSelect='auto';el.readOnly=false;el.disabled=false;});
-
-
-
-
-
-
-
-
-取消复制
-
-
-
-
-
 
 		function c(doc){
 			for(var j =0; j < doc.getElementsByTagName("iframe").length;j++){
@@ -88,6 +39,8 @@ document.onselectstart = function(e) {
 
 
 document.querySelectorAll('input,textarea').forEach(el=>{el.onpaste=e=>e.preventDefault();el.addEventListener('paste',e=>{e.stopImmediatePropagation();e.preventDefault()},true);});
+
+
 
 setTimeout(function(){
 document.querySelectorAll('input,textarea').forEach(el=>{el.onpaste=e=>e.preventDefault();el.addEventListener('paste',e=>{e.stopImmediatePropagation();e.preventDefault()},true);});
